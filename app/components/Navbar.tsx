b0b59ad8f6612c67738f90e8902cdd5eda0465a6
Vivek <vs690799@gmail.com>
2024-10-26 01:43:26 +0530

    updated configuration

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,12 +3,12 @@ const Navbar = () => {
     <>
       <header>
         <nav className="bg-[#052439] text-white">
-          <section className="2xl:w-3/5 w-11/12 h-20  mx-auto flex justify-between items-center">
-            <section className="flex-initial mr-10">
+          <section className="mx-auto flex h-20 w-11/12 items-center justify-between 2xl:w-3/5">
+            <section className="mr-10 flex-initial">
               <h1 className="text-2xl">Engineer Mantra</h1>
             </section>
-            <section className="flex-1 ">
-              <ul className="flex justify-around  w-4/5">
+            <section className="flex-1">
+              <ul className="flex w-4/5 justify-around">
                 <li>Design Services</li>
                 <li>Traning Programm</li>
                 <li>About us</li>
