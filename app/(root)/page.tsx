433b2caca81859da59d55b15a8913fe7de84c7dd
Vivek <vs690799@gmail.com>
2024-10-27 20:28:02 +0530

    added bg image

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,38 @@ import ArchitecturalBanner from "../components/ui/ArchitecturalBanner";
 export default function Home() {
   return (
     <>
-      <section className="opacity-25">
+      <section className="h-full w-full bg-[url('../public/bg-logo.png')] bg-cover bg-center bg-no-repeat backdrop-blur-sm">
+        <p>ajsdf</p>
+        <p>jalkdsj</p>
+        <p>jasgdkla</p>
+        <p>jasdlj</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+        <p>qaksdjg</p>
+      </section>
+      <section className="opacity-20">
         <ArchitecturalBanner />
       </section>
     </>
