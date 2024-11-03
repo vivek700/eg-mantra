cc9df24e4b7ac9f11ae25bd9c48f00971e0dc611
Vivek <vs690799@gmail.com>
2024-11-03 15:44:07 +0530

    updated css

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#052439] text-white">
       <section className="mx-auto flex w-11/12 flex-col items-center pb-14 pt-10 2xl:w-3/5">
-        <button className="mb-5 w-1/3 rounded-lg bg-blue-500 py-2">
+        <button className="mb-5 w-3/4 rounded-lg bg-blue-500 py-2 md:w-1/3">
           Estimate Your Charges
         </button>
         <p className="self-start pb-1">Find Us</p>
@@ -24,7 +24,7 @@ const Footer = () => {
           Ajabpur Khurd, Dehradun, Uttarakhand 248121
         </p>
         <h3 className="self-start pt-4">Contact us:</h3>
-        <p className="self-start">
+        <p className="self-start text-blue-600">
           <a href={"tel:+911354097227"}>+91-1354097227</a>,{" "}
           <a href={"tel:+919897188123"}>+91-9897188123</a>
         </p>
