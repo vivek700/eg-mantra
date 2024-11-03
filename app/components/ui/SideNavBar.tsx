bf315f48cdb3c409660e236eea395aa8d5aeb34f
Vivek <vs690799@gmail.com>
2024-11-03 15:46:21 +0530

    updated code

--- app/components/ui/SideNavBar.tsx
+++ app/components/ui/SideNavBar.tsx
@@ -32,40 +32,95 @@ const SideNavBar = ({
       }));
     }
   };
-  console.log(navOpen);
 
   return (
     <nav className="absolute inset-x-0 top-20 z-10 h-dvh w-full bg-[#052439] p-8 lg:hidden">
       <ul className="flex flex-col gap-4 text-lg">
-        <li className="rounded p-2 hover:bg-slate-700">Design Services</li>
-        <li className="rounded p-2 hover:bg-slate-700">Testing Labs</li>
-        <li className="">
-          <section
-            onClick={() => handleSubNav("training")}
-            className="flex items-center rounded p-2 hover:bg-slate-700"
+        <Link href={"/design-services"}>
+          <li
+            onMouseUp={handleClick}
+            className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/design-services" ? "text-red-400" : ""}`}
           >
-            <span>Training Programm</span>
-            <ChevronDown
-              size={20}
-              className={`text-blue-300 transition-all ease-in-out ${navOpen.training ? "origin-center rotate-180" : ""}`}
-            />
-          </section>
+            Design Services
+          </li>
+        </Link>
+        <Link href={"/testing-labs"}>
+          <li
+            onMouseDown={handleClick}
+            className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/testing-labs" ? "text-red-400" : ""}`}
+          >
+            Testing Labs
+          </li>
+        </Link>
+        <li>
+          <Link href={"/training-programm"}>
+            <section
+              onMouseDown={() => handleSubNav("training")}
+              className={`flex items-center rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/training-programm" ? "text-red-400" : ""}`}
+            >
+              <span>Training Programm</span>
+              <ChevronDown
+                size={20}
+                className={`text-blue-300 transition-all ease-in-out ${navOpen.training ? "origin-center rotate-180" : ""}`}
+              />
+            </section>
+          </Link>
           {navOpen.training && (
             <ul className="px-10 py-1">
-              <li className="rounded p-2 hover:bg-slate-700">
-                Basic Computing
-              </li>
-              <li className="rounded p-2 hover:bg-slate-700">Ms Office</li>
-              <li className="rounded p-2 hover:bg-slate-700">AutoCAD</li>
-              <li className="rounded p-2 hover:bg-slate-700">Revit</li>
-              <li className="rounded p-2 hover:bg-slate-700">Staad.Pro</li>
-              <li className="rounded p-2 hover:bg-slate-700">3Ds Max</li>
+              <Link href={"/training-programm/basic-computing"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/training-programm/basic-computing" ? "text-red-400" : ""}`}
+                >
+                  Basic Computing
+                </li>
+              </Link>
+              <Link href={"/training-programm/ms-office"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/training-programm/ms-office" ? "text-red-400" : ""}`}
+                >
+                  Ms Office
+                </li>
+              </Link>
+              <Link href={"/training-programm/autocad"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/training-programm/autocad" ? "text-red-400" : ""}`}
+                >
+                  AutoCAD
+                </li>
+              </Link>
+              <Link href={"/training-programm/revit"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/training-programm/revit" ? "text-red-400" : ""}`}
+                >
+                  Revit
+                </li>
+              </Link>
+              <Link href={"/training-programm/staad-pro"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/training-programm/staad-pro" ? "text-red-400" : ""}`}
+                >
+                  Staad.Pro
+                </li>
+              </Link>
+              <Link href={"/training-programm/3ds-max"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/training-programm/3ds-max" ? "text-red-400" : ""}`}
+                >
+                  3Ds Max
+                </li>
+              </Link>
             </ul>
           )}
         </li>
-        <li className="">
+        <li>
           <section
-            onClick={() => handleSubNav("company")}
+            onMouseDown={() => handleSubNav("company")}
             className="flex items-center rounded p-2 hover:bg-slate-700"
           >
             <span>Company</span>
@@ -76,10 +131,38 @@ const SideNavBar = ({
           </section>
           {navOpen.company && (
             <ul className="px-10 py-1">
-              <li className="rounded p-2 hover:bg-slate-700">Valuation</li>
-              <li className="rounded p-2 hover:bg-slate-700">Career</li>
-              <li className="rounded p-2 hover:bg-slate-700">about Us</li>
-              <li className="rounded p-2 hover:bg-slate-700">Contact Us</li>
+              <Link href={"/company/valuation"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/company/valuation" ? "text-red-400" : ""}`}
+                >
+                  Valuation
+                </li>
+              </Link>
+              <Link href={"/company/career"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/company/career" ? "text-red-400" : ""}`}
+                >
+                  Career
+                </li>
+              </Link>
+              <Link href={"/company/about-us"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/company/about-us" ? "text-red-400" : ""}`}
+                >
+                  about Us
+                </li>
+              </Link>
+              <Link href={"/company/contact-us"}>
+                <li
+                  onMouseUp={handleClick}
+                  className={`rounded p-2 hover:bg-slate-700 focus:text-red-400 ${pathname === "/company/contact-us" ? "text-red-400" : ""}`}
+                >
+                  Contact Us
+                </li>
+              </Link>
             </ul>
           )}
         </li>
--- app/components/ui/SubNav.tsx
+++ app/components/ui/SubNav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SubNav = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <nav className="invisible absolute z-10 pt-8 text-white group-hover:visible">
-      <ul className="origin-top -translate-y-4 transform rounded-lg bg-[#052439] px-8 py-3 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
+      <ul className="origin-top -translate-y-4 transform rounded-lg border border-white bg-[#052439] px-8 py-3 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
         {children}
       </ul>
     </nav>
