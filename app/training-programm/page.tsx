736a0e2f761d748ba91c339fe01e769182dbf642
Vivek <vs690799@gmail.com>
2024-11-13 23:53:41 +0530

    updated page.tsx

--- app/training-programm/page.tsx
+++ app/training-programm/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const page = () => {
-  return <div>training programm page</div>;
+  return (
+    <>
+      <section>
+        <section>
+          <h2>Skill Development / Training Program</h2>
+          <p>We are determined to improve the quality of engineering.</p>
+          <p>
+            To Achieve this goal we provide high-quality training in CAD and
+            site to our Future engineers.
+          </p>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default page;
