2e059ed4b069c98ffc273637c0636a9bedd94e5c
Vivek <vs690799@gmail.com>
2024-11-19 21:26:04 +0530

    added new component

--- app/components/ui/Buttons.tsx
+++ app/components/ui/Buttons.tsx
@@ -1,12 +1,22 @@
 const Buttons = () => {
   return (
     <>
-      <button className="mx-5 my-2 rounded-lg bg-blue-600 px-4 py-2 text-white">
-        View Samples
-      </button>
-      <button className="mx-5 my-2 rounded-lg bg-blue-600 px-4 py-2 text-white">
-        Estimae Your Charges
-      </button>
+      <a
+        target="_blank"
+        href="https://drive.google.com/drive/folders/1bmcvJA_VVlUIx6otnU51bex6aQZCMmWU"
+      >
+        <button className="mx-5 my-2 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:bg-blue-700">
+          View Samples
+        </button>
+      </a>
+      <a
+        target="_blank"
+        href="https://docs.google.com/spreadsheets/d/1u-QThegW2aHHGnpG38W1d1ghKCqBe4XH0fvKWZK1Lzw/edit?gid=2112575983#gid=2112575983"
+      >
+        <button className="mx-5 my-2 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:bg-blue-700">
+          Estimae Your Charges
+        </button>
+      </a>
     </>
   );
 };
---
+++ app/components/ui/UnderConstruction.tsx
@@ -0,0 +1,23 @@
+import { Settings } from "lucide-react";
+import React from "react";
+
+const UnderConstruction = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50">
+      <div className="rounded-lg p-8 text-center">
+        <Settings className="mx-auto mb-6 h-16 w-16 animate-spin text-blue-800" />
+
+        <h1 className="mb-4 text-3xl font-bold text-gray-800">
+          Under Construction
+        </h1>
+
+        <p className="mx-auto max-w-md text-gray-600">
+          We're working hard to bring you something amazing. This page will be
+          available soon!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UnderConstruction;
